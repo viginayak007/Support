f90e9369588994517fff79bdef1fac6c35d19e4c
JIL-10 <viginayak007@gmail.com>
2018-12-20 16:20:24 +0530

    Added react table

--- src/components/tickets/Tickets.js
+++ src/components/tickets/Tickets.js
@@ -3,6 +3,8 @@ import { getTickets } from '../../store/actions/ticketActions';
 import { connect } from 'react-redux';
 // import { compose } from 'redux'
 // import TicketTD from './TicketTD';
+import ReactTable from "react-table";
+import 'react-table/react-table.css'
 import moment from 'moment'
 
 
@@ -13,49 +15,74 @@ class Tickets extends Component {
     
     render() {
         const { tickets }  = this.props;
+        const columns = [
+            {
+                Header: 'ID',
+                accessor: 'id'
+            },
+            {
+                id: 'createdAt',
+                Header: 'Date',
+                accessor:  d => {
+                    return moment(d.createdAt.nanoseconds).format("YYYY-MM-DD HH:mm:ss");
+                  }
+            },
+            {
+                Header: 'Organization',
+                accessor: 'organization'
+            },
+            {
+                Header: 'User',
+                accessor: 'userId'
+            },
+            {
+                Header: 'Type',
+                accessor: 'id'
+            },
+            {
+                Header: 'Subject',
+                accessor: 'id'
+            },
+            {
+                Header: 'Urgency',
+                accessor: 'urgency'
+            },
+            {
+                Header: 'Impact',
+                accessor: 'impact'
+            },
+            {
+                Header: 'Priority',
+                accessor: 'priority'
+            },
+            {
+                Header: 'Last Updated',
+                accessor: 'id'
+            },
+            {
+                Header: 'Status',
+                accessor: 'status'
+            },
+        ]
         return (
             <div className="container">
-                <div className="row">
-                    <div className="table-responsive" id="tickets-table">
-                        <table className="table table-hover table-striped table-sm">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Date</th>
-                                    <th>Organization</th>
-                                    <th>User</th>
-                                    <th>Type</th>
-                                    <th>Subject</th>
-                                    <th>Urgency</th>
-                                    <th>Impact</th>
-                                    <th>Priority</th>
-                                    <th>Last Updated</th>
-                                    <th>Status</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                { tickets && tickets.map((ticket, i) => {
-                                   return (
-                                            <tr key={i}>
-                                                <td>{ i }</td>
-                                                <td>{ moment(ticket.createdAt.toDate()).calendar() }</td>
-                                                <td>{ ticket.originzation }</td>
-                                                <td>{ ticket.userId }</td>
-                                                <td>{ }</td>
-                                                <td>{ ticket.description }</td>
-                                                <td>{ ticket.urgency }</td>
-                                                <td>{ ticket.impact }</td>
-                                                <td>{ ticket.priority }</td>
-                                                <td></td>
-
-                                            </tr>
-                                    );
-                                })}
-                                
-                            </tbody>
-                        </table>
+            <div className="row">
+                <div className="table-responsive">
+                    <div id="tickets-table">
+                        <ReactTable
+                            data={tickets}
+                            columns={columns}
+                            defaultPageSize = {5}
+                            pageSizeOptions = {[5,10,15, 20]}
+                            className="-striped -highlight -btn-sm"
+                            loading = {tickets.length ? false:true}
+                            collapseOnPageChange = {true}
+                            minRows = {true}
+                        />
                     </div>
                 </div>
+            </div>
+                
             </div>
         );
     }
